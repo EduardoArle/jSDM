b34ff775827780c0fc6637e8b9f892dd4f87bce7
Eduardo Arlé <edu.ariel.arle@gmail.com>
2026-04-18 14:45:00 +0300

    20260418

---
+++ HMSC_tutorial.R
@@ -0,0 +1,9 @@
+#load packages
+library(Hmsc)
+
+#check available vignettes
+vignette(package = "Hmsc")
+
+#download the simplest true jSDM vignette
+vignette("vignette_2_multivariate_low", package = "Hmsc")
+
