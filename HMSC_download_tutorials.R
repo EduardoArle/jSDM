dde8f6f5187dd190ef965de96507052ae2eb81de
Eduardo Arlé <edu.ariel.arle@gmail.com>
2026-04-18 14:50:55 +0300

    20260418

---
+++ HMSC_download_tutorials.R
@@ -0,0 +1,12 @@
+#load HMSC package
+library(Hmsc)
+
+#list available HMSC tutorials
+vignette(package = "Hmsc")
+
+#open univariate tutorial (basic model structure)
+vignette("vignette_1_univariate", package = "Hmsc")
+
+#open simplest multivariate JSDM tutorial (residual correlations)
+vignette("vignette_2_multivariate_low", package = "Hmsc")
+
--- HMSC_tutorial.R
+++
@@ -1,9 +0,0 @@
-#load packages
-library(Hmsc)
-
-#check available vignettes
-vignette(package = "Hmsc")
-
-#download the simplest true jSDM vignette
-vignette("vignette_2_multivariate_low", package = "Hmsc")
-
